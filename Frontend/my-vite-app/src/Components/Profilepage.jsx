b26915a685cdfe7709e77193bc4077faede75b73
Stecy Chirinda <schirinda@meta.com>
2025-06-25 13:56:22 -0700

    I implemented the frontend layout of Profile Page with hard coded data

--- Frontend/my-vite-app/src/Components/Profilepage.jsx
+++ Frontend/my-vite-app/src/Components/Profilepage.jsx
@@ -1,1 +1,18 @@
+import "./Profilepage.css"
 
+
+const Profilepage = () => {
+    return (
+        <div className="profilePage">
+            <h1>Profile Page</h1>
+            <div className="profilePageContent">
+            <img src="https://i.imgur.com/0iZuJXu.png" alt="profile pic" className="profilePic" />
+            <h2>Username</h2>
+            <h3>Major</h3>
+            <h3>Year</h3>
+            <h3>BuddyCount</h3>
+            </div>
+        </div>)
+}
+
+export default Profilepage
