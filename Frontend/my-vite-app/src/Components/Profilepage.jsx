725b2742aae852348b1d3e9d378c11df87c5f130
Stecy Chirinda <schirinda@meta.com>
2025-06-25 14:16:16 -0700

    I implemented the data fetching from backend to display information of specific user on front end

--- Frontend/my-vite-app/src/Components/Profilepage.jsx
+++ Frontend/my-vite-app/src/Components/Profilepage.jsx
@@ -1,18 +1,32 @@
 import "./Profilepage.css"
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {getProfile} from "./fetchingData"
+import NavBar from "./NavBar"
 
 
 const Profilepage = () => {
+    const { id } = useParams();
+    const [profile, setProfile] = useState([]);
+
+    useEffect(() => {
+      const fetchProfile = async () => {
+        const fetchedProfile = await getProfile(id);
+        setProfile(fetchedProfile);
+      };
+      fetchProfile();
+    }, [id]);
     return (
         <div className="profilePage">
+            <NavBar />
             <h1>Profile Page</h1>
             <div className="profilePageContent">
-            <img src="https://i.imgur.com/0iZuJXu.png" alt="profile pic" className="profilePic" />
-            <h2>Username</h2>
-            <h3>Major</h3>
-            <h3>Year</h3>
-            <h3>BuddyCount</h3>
+            <img src={profile.profilePicture} alt="profile pic" className="profilePic" />
+            <h2>{profile.name}{'  '}{profile.surname}</h2>
+            <h3>{profile.major}</h3>
+            <h3>{profile.classification}</h3>
+            <h3>{profile.walkCount}</h3>
             </div>
         </div>)
 }
-
 export default Profilepage
