ba462d35b95a7341e664b1564b547ceab4d9e0aa
Stecy Chirinda <schirinda@meta.com>
2025-06-27 09:36:49 -0700

    - I set up the Verification page. - I implemented input to be block (each code digit in its own input block)

--- Frontend/my-vite-app/src/Components/VerificationCodePage.jsx
+++ Frontend/my-vite-app/src/Components/VerificationCodePage.jsx
@@ -2,6 +2,17 @@ const VerificationCodePage = () => {
     return(
         <div>
             <h1>Verification Code Page</h1>
+            <section >
+            <label>Enter Verification Code: </label>
+            <br/>
+            <input type="tel" maxlength="1" inputmode="numeric"/>
+            <input type="tel" maxlength="1" inputmode="numeric"/>
+            <input type="tel" maxlength="1" inputmode="numeric"/>
+            <input type="tel" maxlength="1" inputmode="numeric"/>
+            <input type="tel" maxlength="1" inputmode="numeric"/>
+            <br/>
+            <button>Verify</button>
+            </section>
         </div>
     )
 }
