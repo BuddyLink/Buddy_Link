34e7505eb0ed1a177d26cdf781a193dbfd94d619
Stecy Chirinda <schirinda@meta.com>
2025-07-15 21:33:07 -0700

    I removed white space between Number and arguments. I added a condition to the setTimer to only set when time > 0. I reordered to ensure intervalTime and timeUpdate are not redefined every time I run useEffect.

--- Frontend/my-vite-app/src/Components/Timer.jsx
+++ Frontend/my-vite-app/src/Components/Timer.jsx
@@ -2,22 +2,23 @@ import { useState, useEffect } from "react";
 
 const Timer = ({ duration }) => {
   const [time, setTime] = useState(duration);
+  const intervalTime = 1000;
+  const timeUpdate = () => {
+    if(time > 0){
+    setTime(time - intervalTime)};
+  };
   useEffect(() => {
-    const intervalTime = 1000
-    const timeUpdate = () => {
-      setTime(time - intervalTime)
-    };
     setTimeout(timeUpdate, intervalTime);
   }, [time]);
-  return <p>{timeFormat(time)}</p>
+  return <p>{timeFormat(time)}</p>;
 };
 
 const timeFormat = (time) => {
-    let total_seconds =  Number (Math.floor(time/1000))
-    let total_minutes =  Number (Math.floor(total_seconds/60))
-    let seconds = Number (total_seconds % 60)
+  let total_seconds = Number(Math.floor(time / 1000));
+  let total_minutes = Number(Math.floor(total_seconds / 60));
+  let seconds = Number(total_seconds % 60);
 
-    return `Minutes: ${total_minutes} Seconds: ${seconds}`
-}
+  return `Minutes: ${total_minutes} Seconds: ${seconds}`;
+};
 
 export default Timer;
